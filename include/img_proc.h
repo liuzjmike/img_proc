04481fe860fb40065b59be8b97faf423cff4e1bf
Mike Liu <zijie.liu@duke.edu>
2017-02-19 21:59:07 -0500

    Make computeCost a service. Bug in the program

--- include/img_proc.h
+++ include/img_proc.h
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
 #include "std_srvs/Trigger.h"
+#include "img_proc/ComputeCost.h"
 #include "image_transport/image_transport.h"
 #include "cv_bridge/cv_bridge.h"
 
@@ -27,32 +28,36 @@ namespace img_proc
 
     void listenFeature(const std::string& imageTopic, const std::string& jointTopic);
     void stopListenFeature();
-    bool triggerCallback(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
-    float computeCost(const std::string& imageTopic, const std::string& jointTopic);
 
   private:
+    bool triggerCallback(std_srvs::TriggerRequest& req, std_srvs::TriggerResponse& res);
+    bool computeCost(img_proc::ComputeCost::Request& req, img_proc::ComputeCost::Response& res);
     void featureImageCallback(const sensor_msgs::ImageConstPtr& msg);
     void featureJointCallback(const sensor_msgs::JointStateConstPtr& msg);
     void queryImageCallback(const sensor_msgs::ImageConstPtr& msg);
-    void publishJointState();
+    void publishJointState(const std::string& jointTopic);
     std::vector<int> convertImage(const sensor_msgs::ImageConstPtr& msg);
+    bool samePosition(std::vector<double> position1, std::vector<double> position2);
 
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
     image_transport::Subscriber img_sub_;
-    ros::ServiceServer service_;
+    ros::ServiceServer record_service_, compare_service_;
     ros::Subscriber joint_sub_;
     ros::Publisher joint_pub_;
 
     std::vector<flann::Index<flann::L2<int> > > indices_;
     std::vector<sensor_msgs::JointState> joint_states_;
 
-    std::string trigger_topic_;
     int record_seq_, broadcast_seq_, lo_thresh_, hi_thresh_;
     float error_;
     bool image_flag_, joint_flag_;
+    std::vector<double> position_;
 
     flann::IndexParams params_;
+
+    const double threshold = 0.001;
+    const std::string record_topic = "record_feature", compare_topic = "compute_cost";
   };
 }
 
--- src/img_proc.cpp
+++ src/img_proc.cpp
@@ -67,12 +67,12 @@ namespace img_proc
   ImageProcessor::ImageProcessor(const IndexParams& params, const int loThresh, const int hiThresh)
     : it_(nh_)
     , record_seq_(0)
-    , trigger_topic_("record_feature")
     , lo_thresh_(loThresh)
     , hi_thresh_(hiThresh)
   {
     params_ = params;
-    service_ = nh_.advertiseService(trigger_topic_, &ImageProcessor::triggerCallback, this);
+    record_service_ = nh_.advertiseService(record_topic, &ImageProcessor::triggerCallback, this);
+    compare_service_ = nh_.advertiseService(compare_topic, &ImageProcessor::computeCost, this);
   }
 
   void ImageProcessor::listenFeature(const string& imageTopic, const string& jointTopic)
@@ -93,7 +93,6 @@ namespace img_proc
     res.success = img_sub_ != NULL && img_sub_.getTopic().size() > 0;
     if(res.success)
     {
-      image_flag_ = true;
       joint_flag_ = true;
       while(image_flag_ || joint_flag_);
       res.message = "Information recorded";
@@ -105,16 +104,16 @@ namespace img_proc
     return true;
   }
 
-  float ImageProcessor::computeCost(const string& imageTopic, const string& jointTopic)
+  bool ImageProcessor::computeCost(img_proc::ComputeCost::Request& req, img_proc::ComputeCost::Response& res)
   {
     broadcast_seq_ = 0;
     error_ = 0;
     image_flag_ = false;
-    joint_pub_ = nh_.advertise<sensor_msgs::JointState>(jointTopic, 2);
-    img_sub_ = it_.subscribe(imageTopic, 2, &ImageProcessor::featureImageCallback, this);
-    std::thread t(&ImageProcessor::publishJointState, this);
+    std::thread t(&ImageProcessor::publishJointState, this, req.joint_topic);
+    img_sub_ = it_.subscribe(req.image_topic, 2, &ImageProcessor::queryImageCallback, this);
     t.join();
-    return error_;
+    res.cost = error_ / broadcast_seq_;
+    return true;
   }
 
   void ImageProcessor::featureImageCallback(const sensor_msgs::ImageConstPtr& msg)
@@ -133,8 +132,12 @@ namespace img_proc
   {
     if(joint_flag_)
     {
-      joint_states_.push_back(*msg);
-      joint_flag_ = false;
+      if(samePosition(position_, msg->position)) {
+        joint_states_.push_back(*msg);
+        joint_flag_ = false;
+        image_flag_ = true;
+      }
+      position_ = msg->position;
     }
   }
 
@@ -143,19 +146,26 @@ namespace img_proc
     if(image_flag_)
     {
       error_ += diff(indices_[broadcast_seq_], convertImage(msg), 64);
+      std::cout << error_ << std::endl;
       image_flag_ = false;
     }
     //TODO: Make radius an argument
   }
 
-  void ImageProcessor::publishJointState()
+  void ImageProcessor::publishJointState(const string& jointTopic)
   {
-    while(broadcast_seq_ < record_seq_)
+    joint_pub_ = nh_.advertise<sensor_msgs::JointState>(jointTopic, 2);
+    while(broadcast_seq_ < record_seq_-1)
     {
-      joint_pub_.publish(joint_states_[broadcast_seq_]);
-      image_flag_ = true;
-      while(image_flag_);
-      broadcast_seq_++;
+      if(!image_flag_)
+      {
+        std::cout << broadcast_seq_ << std::endl;
+        sensor_msgs::JointState state = joint_states_[broadcast_seq_];
+        state.header.stamp = ros::Time::now();
+        joint_pub_.publish(state);
+        image_flag_ = true;
+        broadcast_seq_++;
+      }
     }
   }
 
@@ -173,4 +183,34 @@ namespace img_proc
     Mat edgeImg = detectEdge(cv_ptr->image, lo_thresh_, hi_thresh_);
     return locateEdge(edgeImg);
   }
+
+  bool ImageProcessor::samePosition(vector<double> position1, vector<double> position2)
+  {
+    if(position1.size() != position2.size())
+    {
+      return false;
+    }
+    else
+    {
+      for(int i = 0; i < position1.size(); i++)
+      {
+        if(std::abs(position1[i] - position2[i]) > threshold)
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "img_proc");
+  img_proc::ImageProcessor ip;
+  ip.listenFeature(argv[1], argv[2]);
+  ros::AsyncSpinner spinner(2);
+  spinner.start();
+  ros::waitForShutdown();
+  return 0;
 }
