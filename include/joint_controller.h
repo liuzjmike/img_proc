f9c5715b19bbad7729504241644e14d9541b0798
Mike Liu <zijie.liu@duke.edu>
2017-02-19 21:04:08 -0500

    Finish joint controller

--- include/joint_controller.h
+++ include/joint_controller.h
@@ -1,4 +1,8 @@
 #include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "std_srvs/Trigger.h"
+
+#include <cmath>
 
 #ifndef JOINT_CONTROLLER_H
 #define JOINT_CONTROLLER_H
@@ -8,12 +12,19 @@ namespace img_proc
   class JointController
   {
   public:
-    JointController();
+    JointController(std::string prefix, std::vector<std::string> joints);
+
+    void fullRecord(int numPerJoint = 30);
 
   private:
-    ros::Subscriber joint_sub_;
-    ros::Publisher joint_pub_;
-  }
+    void fullRecord(int numPerJoint, int index);
+
+    const std::string topic_suffix_ = "_position_controller/command";
+    const double pi = 3.14159;
+    ros::NodeHandle nh_;
+    ros::ServiceClient client_;
+    std::vector<ros::Publisher> joint_pub_;
+  };
 }
 
 #endif
